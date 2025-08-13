49d5530767b5fc7d1d236fd0edd8eeb201e1f0b8
Snjothekja <37817826+Snjothekja@users.noreply.github.com>
2025-08-12 17:46:44 -0700

    Starting Mod Files

---
+++ src/client/java/dev/sjnothekja/gittokubejs/client/GittokubejsClient.java
@@ -0,0 +1,10 @@
+package dev.sjnothekja.gittokubejs.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GittokubejsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/sjnothekja/gittokubejs/Gittokubejs.java
@@ -0,0 +1,10 @@
+package dev.sjnothekja.gittokubejs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gittokubejs implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
